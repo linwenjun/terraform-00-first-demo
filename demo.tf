278218f6c026554f47f4015b4f08696863858ab0
linwenjun <linwenjun@163.com>
2018-11-12 14:45:08 +0800

    first commit

---
+++ demo.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "cn-northwest-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-a1ccd8c3"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "example"
+  }
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
+
+output "ip" {
+  value = "${aws_instance.example.public_ip}"
+}
